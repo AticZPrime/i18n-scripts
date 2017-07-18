2d6db046487caec00dd9328a6ee08cb47423bb20
AticZPrime <ezitoregaming@gmail.com>
2017-07-18 03:18:15 -0400

    Create portuguese.lua
    PT-BR translation for blogs.

---
+++ portuguese.lua
@@ -0,0 +1,246 @@
+{
+
+	permission_failure = "Você atualmente não tem permissão para fazer isso.",
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Mode de Administrador",
+
+	-- The button to view all logs stored.
+	all_logs = "Todos os Logs",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Todos os Jogadores",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Jogadores",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Obtendo dados, por favor espere...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Sem dados!",
+
+	advanced_search = "Pesquisa Avançada",
+	quick_search = "Pesquisa Rapida",
+	search = "Pesquisa",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "Localizador de SteamID",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Selecione alguma coisa",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Pule uma página",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Pule para página...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Digite um número de uma página(existem %d páginas)",
+
+	cancel = "Cancelar",
+
+	error_over_pagenum = "Essa página de logs não existe",
+	error_not_num = "Isso não é um número",
+
+	error = "Erro",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Use o clique com o botão direito do mouse para mais opções",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Veja o Logs do Jogador",
+
+	-- Copy [something] [to clipboard]
+	copy = "Copie %s",
+
+	profile = "Perfil do Steam",
+
+	left_click_to_copy = "Botão direito do mouse para copiar",
+
+	modules = "Módulos",
+
+	custom = "Personalização",
+
+	enter_a_steamid = "Insira um SteamID...",
+
+	error_not_steamid = "Isso não é um SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "E contém...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Modo de Cor",
+
+	jump_to_date = "Pule para a data",
+
+	jump_to_date_tooltip = "Botão direito do mouse para Pule para a data/tempo",
+
+	type_date = "Insira uma data. Tem que ser neste formato: DD/MM/YYYY (%s/%s/%s)",
+
+	error_not_date = "Isso não foi uma data válida. Tem que ser neste formato: DD/MM/YYYY (%s/%s/%s)",
+
+	type_time = "Insira um tempo. Tem que ser neste formato: HH:MM (%s:%s) (24 horas)\n Deixe em branco para ignorar o tempo.",
+
+	error_not_time = "Isso não foi um tempo válido. Tem que ser neste formato: HH:MM (%s:%s) (24 horas)",
+
+	archive = "Arquivo",
+
+	searching = "Procurando",
+
+	search_archive = "Procure um arquivo",
+
+	search_archive_warning = "Procurando um arquivo pode atrasar os bancos de dados locais do servidor. Use apenas se necessário.",
+
+	logging = "Logging",
+
+	save = "Salvar",
+
+	enabled = "Ativado",
+
+	disabled = "Desativado",
+
+	disable = "Desativar",
+
+	enable = "Permitir",
+
+	give = "Dar",
+
+	take = "Tomar",
+
+	new_steamid = "Novo SteamID",
+
+	new_usergroup = "Novo Usergroup",
+
+	usergroup_name = "Nome do Usergroup",
+
+	-- "Delete this entry from permissions"
+	delete_entry = "Deletar entrada",
+	
+}
+{
+
+	permission_failure = "Você atualmente não tem permissão para fazer isso.",
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Mode de Administrador",
+
+	-- The button to view all logs stored.
+	all_logs = "Todos os Logs",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Todos os Jogadores",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Jogadores",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Obtendo dados, por favor espere...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Sem dados!",
+
+	advanced_search = "Pesquisa Avançada",
+	quick_search = "Pesquisa Rapida",
+	search = "Pesquisa",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "Localizador de SteamID",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Selecione alguma coisa",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Pule uma página",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Pule para página...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Digite um número de uma página(existem %d páginas)",
+
+	cancel = "Cancelar",
+
+	error_over_pagenum = "Essa página de logs não existe",
+	error_not_num = "Isso não é um número",
+
+	error = "Erro",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Use o clique com o botão direito do mouse para mais opções",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Veja o Logs do Jogador",
+
+	-- Copy [something] [to clipboard]
+	copy = "Copie %s",
+
+	profile = "Perfil do Steam",
+
+	left_click_to_copy = "Botão direito do mouse para copiar",
+
+	modules = "Módulos",
+
+	custom = "Personalização",
+
+	enter_a_steamid = "Insira um SteamID...",
+
+	error_not_steamid = "Isso não é um SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "E contém...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Modo de Cor",
+
+	jump_to_date = "Pule para a data",
+
+	jump_to_date_tooltip = "Botão direito do mouse para Pule para a data/tempo",
+
+	type_date = "Insira uma data. Tem que ser neste formato: DD/MM/YYYY (%s/%s/%s)",
+
+	error_not_date = "Isso não foi uma data válida. Tem que ser neste formato: DD/MM/YYYY (%s/%s/%s)",
+
+	type_time = "Insira um tempo. Tem que ser neste formato: HH:MM (%s:%s) (24 horas)\n Deixe em branco para ignorar o tempo.",
+
+	error_not_time = "Isso não foi um tempo válido. Tem que ser neste formato: HH:MM (%s:%s) (24 horas)",
+
+	archive = "Arquivo",
+
+	searching = "Procurando",
+
+	search_archive = "Procure um arquivo",
+
+	search_archive_warning = "Procurando um arquivo pode atrasar os bancos de dados locais do servidor. Use apenas se necessário.",
+
+	logging = "Logging",
+
+	save = "Salvar",
+
+	enabled = "Ativado",
+
+	disabled = "Desativado",
+
+	disable = "Desativar",
+
+	enable = "Permitir",
+
+	give = "Dar",
+
+	take = "Tomar",
+
+	new_steamid = "Novo SteamID",
+
+	new_usergroup = "Novo Usergroup",
+
+	usergroup_name = "Nome do Usergroup",
+
+	-- "Delete this entry from permissions"
+	delete_entry = "Deletar entrada",
+	
+}
